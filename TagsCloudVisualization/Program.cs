1c7d95380fa4eaabc005ecd0d9963a8a661bee09
Ollisteka <zhukova.o.m@yandex.ru>
2017-10-27 23:30:34 +0500

    Initial Commit

---
+++ TagsCloudVisualization/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+	class CircularCloudLayouter
+	{
+		public CircularCloudLayouter(Point center)
+		{
+			
+		}
+
+		public Rectangle PutNextRectangle(Size rectangleSize)
+		{
+			throw new NotImplementedException();
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
