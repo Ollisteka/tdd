fc75eacd6ac66161512fd9f330cb29fd6f06da4b
Ollisteka <zhukova.o.m@yandex.ru>
2017-11-28 08:05:35 +0500

    Подключила Docopt

--- TagsCloudVisualization/Program.cs
+++ TagsCloudVisualization/Program.cs
@@ -6,73 +6,63 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using Autofac;
+using DocoptNet;
 using NHunspell;
+using TagsCloudVisualization.Interfaces;
 
 namespace TagsCloudVisualization
 {
-	public class AutofacConfig
-	{
-		public static void ConfigureContainer()
-		{
-			// получаем экземпляр контейнера
-			var builder = new ContainerBuilder();
-
-			// регистрируем контроллер в текущей сборке
-			//builder.(typeof(Program).Assembly);
-
-			// регистрируем споставление типов
-			//builder.RegisterType<BookRepository>().As<IRepository>();
-
-			// создаем новый контейнер с теми зависимостями, которые определены выше
-			var container = builder.Build();
-
-			// установка сопоставителя зависимостей
-			//DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
-		}
-	}
 	internal static class Program
 	{
-		public static LayoutForm CreateForm()
-		{
-			throw new NotImplementedException();
-		}
+		private const string usage = @"Tags Cloud Visualization.
 
-		private static Dictionary<string, int> ProccessWords(string filename)
-		{
-			var content = File.ReadAllText(filename);
+	Usage:
+	  TagsCloudVisualization.exe <inputfile>
+	  TagsCloudVisualization.exe [-o FILE] <inputfile>
+	  TagsCloudVisualization.exe <inputfile>  [--top=<integer>]
+	  TagsCloudVisualization.exe (-h | --help)
 
-			using (var hunspell = new Hunspell("dictionaries/ru_RU.aff", "dictionaries/ru_RU.dic"))
-			{
-				return Regex.Split(content, @"[^\p{L}]*\p{Z}[^\p{L}]*")
-					.Where(x => x.Length > 3)
-					.Select(x =>
-					{
-						var word = x.ToLower();
-						//var morphs = hunspell.Analyze(word);
-						var stems = hunspell.Stem(word);
-						return stems.Any() ? stems[0] : word;
-					})
-					.GroupBy(x => x)
-					.Select(x => new
-					{
-						KeyField = x.Key,
-						Count = x.Count()
-					})
-					.OrderByDescending(x => x.Count)
-					.Take(100).ToDictionary(key => key.KeyField, val => val.Count);
-			}
-		}
+	Options:
+	  -o FILE  specify output file. 
+	  --top=<integer> specify how many words to show [default: 70].
+	  -h --help     Show this screen.
 
+	";
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		private static void Main()
+		private static void Main(string[] args)
 		{
-			var inputFile = @"books/harry.txt";
+
+			var arguments = new Docopt().Apply(usage, args, exit: true);
+			var a = arguments["-o"] == null;
+			foreach (var argument in arguments)
+			{
+				Console.WriteLine("{0} = {1}", argument.Key, argument.Value);
+			}
+
+			var inputFile = arguments["<inputfile>"].ToString();
+			var topWords = arguments["--top"].AsInt;
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new LayoutForm(ProccessWords(inputFile)));
+			Application.Run(CreateForm(inputFile, topWords));
+			//Application.Run(new LayoutForm(ProccessWords(inputFile)));
 		}
+
+		public static LayoutForm CreateForm(string filename, int top)
+		{
+			var container = new ContainerBuilder();
+			//container.RegisterType<LayoutForm>().AsSelf();
+			container.RegisterType<LayoutForm>().AsSelf();
+			container.RegisterType<FileHandler>()
+				.As<ITextHandler>()
+				.WithParameter("filename", filename);
+			container.RegisterType<CloudDrawer>().As<ICloudDrawer>();
+			var build = container.Build();
+			return build.Resolve<LayoutForm>();
+		}
+
+		
 	}
 }
