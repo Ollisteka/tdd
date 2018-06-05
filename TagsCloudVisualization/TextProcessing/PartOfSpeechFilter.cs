d216178ce92a38d735cb1cabf93b0c9c75ce4e97
Ollisteka <zhukova.o.m@yandex.ru>
2018-06-05 21:17:04 +0500

    Добавлен фильтр по частям речи (пока только на английском)

---
+++ TagsCloudVisualization/TextProcessing/PartOfSpeechFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SharpNL.POSTag;
+using TagsCloudVisualization.Interfaces;
+
+namespace TagsCloudVisualization.TextProcessing
+{
+    public class PartOfSpeechFilter : ITextFilter
+    {
+        private readonly HashSet<char> whiteList = new HashSet<char> {'N', 'J', 'V'};
+
+        public IEnumerable<string> Filter(IEnumerable<string> content)
+        {
+            var solutiondir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            var binFile = Path.Combine(solutiondir, "dictionaries", "en-pos-maxent.bin");
+            POSModel posModel = null;
+            try
+            {
+                using (var modelFile = new FileStream(binFile, FileMode.Open))
+                {
+                    posModel = new POSModel(modelFile);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong with the POSFilter:");
+                LayoutApp.ExitWithError(e.Message);
+            }
+
+            var posTagger = new POSTaggerME(posModel);
+            foreach (var word in content)
+            {
+                var tags = posTagger.Tag(new[] {word});
+                if (whiteList.Contains(tags[0][0]))
+                    yield return word;
+            }
+        }
+    }
+}
